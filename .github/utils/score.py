ebcc10f81f86153ed0774b3019292b748fb0ab12
Neea <neearusch@gmail.com>
2024-06-28 09:48:12 -0400

    better scores

--- .github/utils/score.py
+++ .github/utils/score.py
@@ -6,10 +6,12 @@ from types import SimpleNamespace as Sn
 def main(in_file):
     with open(in_file) as fp:
         data = json.load(fp, object_hook=lambda d: Sn(**d))
-    rank, team, pts = next(
-        ((row.values + [0])[:3] for row in data.rows
+    values = next(
+        (row.values for row in data.rows
          if row.isYou), ('Team unknown', 999, 0))
-    print(f'{team} is #{rank} with score {pts}')
+    rank, team = values[:2]
+    scores= " ".join([str(s) if s else '-' for s in values[2:]])
+    print(f'{team} is #{rank} with scores: {scores}')
 
 
 if __name__ == '__main__':
