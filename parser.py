95a6092828409ca321fe3b3fe1017853f284a0b4
Neea <neearusch@gmail.com>
2024-06-29 01:54:47 -0400

    lambdaman 10

--- parser.py
+++ parser.py
@@ -16,13 +16,13 @@ whitespace = regex(r'\s*')
 @generate
 def boolean():
     result = yield string('T') | string('F')
-    return ('bool', result == 'T')
+    return 'bool', result == 'T'
 
 @generate
 def integer():
     yield string('I')
     digits = yield regex(r'[!-~]+')
-    return ('int', to_int(digits))
+    return 'int', to_int(digits)
 
 @generate
 def string_literal():
@@ -35,15 +35,15 @@ def unary_op():
     yield string('U')
     op = yield regex(r'[-!#$]')
     arg = yield icfp_expr
-    return ('unary', op, arg)
+    return 'unary', op, arg
 
 @generate
 def binary_op():
     yield string('B')
-    op = yield regex(r'[+\-*/%<>=|&.TD$]')
+    op = yield regex(r'[+\-*/%<>=&.TD$|]')
     left = yield icfp_expr
     right = yield icfp_expr
-    return ('binary', op, left, right)
+    return 'binary', op, left, right
 
 @generate
 def if_statement():
@@ -51,20 +51,20 @@ def if_statement():
     cond = yield icfp_expr
     true_branch = yield icfp_expr
     false_branch = yield icfp_expr
-    return ('if', cond, true_branch, false_branch)
+    return 'if', cond, true_branch, false_branch
 
 @generate
 def lambda_abstraction():
     yield string('L')
-    var_num = yield regex(r'[!-~]+')
+    var_num = yield regex(r'[!-~"#$]*')
     body = yield icfp_expr
-    return ('lambda', to_int(var_num), body)
+    return 'lambda', to_int(var_num), body
 
 @generate
 def variable():
     yield string('v')
-    var_num = yield regex(r'[!-~]+')
-    return ('var', to_int(var_num))
+    var_num = yield regex(r'[!-~#$"]+')
+    return 'var', to_int(var_num)
 
 # Main ICFP parser
 @generate
@@ -151,7 +151,9 @@ if __name__ == "__main__":
         """B$ B$ L# L$ v# B. SB%,,/ S}Q/2,$_ IK""",
         """B$ B$ L# B$ L# B$ v# B$ v# v# L# B$ v# B$ v# v# L# L# ? B= v# I! I# B$ L$ B+ B$ v# v$ B$ v# v$ B- v# I# I%""",
         """? B= B$ B$ B$ B$ L$ L$ L$ L# v$ I" I# I$ I% I$ ? B= B$ L$ v$ I+ I+ ? B= BD I$ S4%34 S4 ? B= BT I$ S4%34 S4%3 ? B= B. S4% S34 S4%34 ? U! B& T F ? B& T T ? U! B| F F ? B| F T ? B< U- I$ U- I# ? B> I$ I# ? B= U- I" B% U- I$ I# ? B= I" B% I( I$ ? B= U- I" B/ U- I$ I# ? B= I# B/ I( I$ ? B= I' B* I# I$ ? B= I$ B+ I" I# ? B= U$ I4%34 S4%34 ? B= U# S4%34 I4%34 ? U! F ? B= U- I$ B- I# I& ? B= I$ B- I& I# ? B= S4%34 S4%34 ? B= F F ? B= I$ I$ ? T B. B. SM%,&k#(%#+}IEj}3%.$}z3/,6%},!.'5!'%y4%34} U$ B+ I# B* I$> I1~s:U@ Sz}4/}#,!)-}0/).43}&/2})4 S)&})3}./4}#/22%#4 S").!29}q})3}./4}#/22%#4 S").!29}q})3}./4}#/22%#4 S").!29}q})3}./4}#/22%#4 S").!29}k})3}./4}#/22%#4 S5.!29}k})3}./4}#/22%#4 S5.!29}_})3}./4}#/22%#4 S5.!29}a})3}./4}#/22%#4 S5.!29}b})3}./4}#/22%#4 S").!29}i})3}./4}#/22%#4 S").!29}h})3}./4}#/22%#4 S").!29}m})3}./4}#/22%#4 S").!29}m})3}./4}#/22%#4 S").!29}c})3}./4}#/22%#4 S").!29}c})3}./4}#/22%#4 S").!29}r})3}./4}#/22%#4 S").!29}p})3}./4}#/22%#4 S").!29}{})3}./4}#/22%#4 S").!29}{})3}./4}#/22%#4 S").!29}d})3}./4}#/22%#4 S").!29}d})3}./4}#/22%#4 S").!29}l})3}./4}#/22%#4 S").!29}N})3}./4}#/22%#4 S").!29}>})3}./4}#/22%#4 S!00,)#!4)/.})3}./4}#/22%#4 S!00,)#!4)/.})3}./4}#/22%#4""",
-        """U$ B+ I# B* I$> I1~s:U@"""
+        """U$ B+ I# B* I$> I1~s:U@""",
+        """B. SF B$ B$ L" B$ L" B$ L# B$ v" B$ v# v# L# B$ v" B$ v# v# L$ L# ? B= v# I" v" B. v" B$ v$ B- v# I" Sl I#,""",
+        """B$ L+ B. B. SF B$ B$ v+ Sl IR B$ B$ v+ B. S~ B$ B$ v+ Sl IS IR L" B$ L" B$ L# B$ v" B$ v# v# L# B$ v" B$ v# v# L$ L# ? B= v# I" v" B. v" B$ v$ B- v# I\""""
     ]
 
     for i, case in enumerate(test_cases, 1):
